60e42283c8fb82d0f53918ace519f769a5815e3b
blakeotto57 <botto@ucsc.edu>
2024-11-08 00:45:30 -0800

    added more to options of personal care

---
+++ lib/models/personal_care_options.dart
@@ -0,0 +1,180 @@
+import 'package:flutter/material.dart';
+
+import 'package:treehouse/pages/category_pages/personal_care.dart';
+import 'package:treehouse/pages/category_pages/academic_assistance.dart';
+import 'package:treehouse/pages/category_pages/cleaning.dart';
+import 'package:treehouse/pages/category_pages/errands_moving.dart';
+import 'package:treehouse/pages/category_pages/pet_care.dart';
+import 'package:treehouse/pages/category_pages/photography.dart';
+import 'package:treehouse/pages/category_pages/technical_services.dart';
+import 'package:treehouse/pages/category_pages/vending_cooking.dart';
+
+
+
+
+//folder that distingushes the categories from one another
+class CategoryModel {
+  final String name;
+  final String iconPath;
+  final Color boxColor;
+  final Function (BuildContext) onTap;
+
+  CategoryModel({
+    required this.name,
+    required this.iconPath,
+    required this.boxColor,
+    required this.onTap,
+    });
+
+    static List<CategoryModel> getCategories() {
+      List<CategoryModel> categories = [];
+
+
+
+    //Personal Care box
+    categories.add(
+      CategoryModel(
+        name: "Personal Care", 
+        iconPath: "assets/icons/haircut-icon.svg", 
+        boxColor: Color.fromRGBO(75, 57, 239, 1),
+
+        onTap: (context) {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => PersonalCarePage()),
+           );
+          }
+        ),
+      );
+
+
+
+
+    //Vending & Cooking box
+    categories.add(
+      CategoryModel(
+        name: "Vending & Cooking", 
+        iconPath: "assets/icons/vending-icon.svg", 
+        boxColor: Color.fromRGBO(215, 57, 239, 1),
+
+        onTap: (context) {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => VendingCookingPage()),
+            );
+          }
+        ),
+      );
+
+
+
+    //Photography box
+    categories.add(
+      CategoryModel(
+        name: "Photography", 
+        iconPath: "assets/icons/camera-icon.svg", 
+        boxColor: Color.fromRGBO(57, 210, 192, 1),
+
+        onTap: (context) {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => PhotopgraphyPage()),
+            );
+          }
+        ),
+      );
+
+
+
+    //Academic assitance box
+    categories.add(
+      CategoryModel(
+        name: "Academic Assistance", 
+        iconPath: "assets/icons/school-icon.svg", 
+        boxColor: Color.fromRGBO(238, 138, 96, 1),
+
+        onTap: (context) {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => AcademicAssistancePage()),
+            );
+
+          }
+        ),
+      );
+
+
+
+    //Technical Services box
+    categories.add(
+      CategoryModel(
+        name: "Technical Services", 
+        iconPath: "assets/icons/computer-icon.svg", 
+        boxColor: Color.fromRGBO(255, 64, 129, 1),
+
+        onTap: (context) {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => TechnicalServicesPage()),
+            );
+          }
+        ),
+      );
+
+
+
+     //Errands & Moving Services box
+    categories.add(
+      CategoryModel(
+        name: "Errands & Moving", 
+        iconPath: "assets/icons/box-icon.svg", 
+        boxColor: Color.fromRGBO(255, 193, 7, 1),
+
+        onTap: (context) {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => ErrandsMovingPage()),
+            );
+          }
+        ),
+      );
+
+
+
+    //Pet Care box
+    categories.add(
+      CategoryModel(
+        name: "Pet Care", 
+        iconPath: "assets/icons/dog-icon.svg", 
+        boxColor: Color.fromRGBO(76, 175, 80, 1),
+
+        onTap: (context) {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => PetCarePage()),
+            );
+          }
+        ),
+      );
+
+
+
+    //Cleaning box
+    categories.add(
+      CategoryModel(
+        name: "Cleaning", 
+        iconPath: "assets/icons/clean-icon.svg", 
+        boxColor: Color.fromRGBO(156, 39, 176, 1),
+
+        onTap: (context) {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => CleaningPage()),
+            );
+          }
+        ),
+      );
+
+    return categories;
+    }
+}
--- lib/pages/category_pages/personal_care.dart
+++ lib/pages/category_pages/personal_care.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:treehouse/models/personal_care_options.dart';
+
+
+
 
 class PersonalCarePage extends StatelessWidget {
   @override
@@ -7,9 +12,122 @@ class PersonalCarePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Personal Care Page'),
       ),
-      body: Center(
-        child: Text('Welcome to My New Page!'),
+      body: PersonalCareContent(
+        categories: [
+          // Add CategoryModel objects here as needed
+        ],
       ),
     );
   }
+}
+
+class PersonalCareContent extends StatelessWidget {
+  final List<CategoryModel> categories;
+
+  PersonalCareContent({required this.categories});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(height: 10), // Space between AppBar and content
+          // Add search bar or other widgets here if needed
+          SizedBox(height: 10),
+          Container(
+            color: Colors.white,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start, // Align categories title to the left
+              children: [
+                const Padding(
+                  padding: EdgeInsets.only(left: 20.0),
+                  child: Text(
+                    "Categories",
+                    style: TextStyle(
+                      color: Colors.black, // Color of categories text
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  child: Divider(
+                    color: Colors.grey,
+                    thickness: 2.0,
+                  ),
+                ),
+                SizedBox(height: 1), // Space between categories label and list
+                Container(
+                  height: 361, // Height of category list view
+                  color: Colors.white, // Background color of category list
+                  child: ListView.separated(
+                    itemCount: categories.length,
+                    scrollDirection: Axis.vertical,
+                    padding: EdgeInsets.all(10),
+                    separatorBuilder: (context, index) => SizedBox(height: 25), // Space between categories
+                    itemBuilder: (context, index) {
+                      return InkWell(
+                        onTap: () => categories[index].onTap(context),
+                        child: Container(
+                          height: 50, // Height of each category box
+                          decoration: BoxDecoration(
+                            color: categories[index].boxColor.withOpacity(0.5),
+                            borderRadius: BorderRadius.circular(16),
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.all(10.0),
+                                child: Align(
+                                  alignment: Alignment.centerLeft,
+                                  child: Text(
+                                    categories[index].name,
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white, // Font color in category boxes
+                                      fontSize: 15,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: SvgPicture.asset(
+                                  categories[index].iconPath,
+                                  width: 40,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+// Placeholder for CategoryModel class
+class CategoryModel {
+  final String name;
+  final String iconPath;
+  final Color boxColor;
+  final Function(BuildContext) onTap;
+
+  CategoryModel({
+    required this.name,
+    required this.iconPath,
+    required this.boxColor,
+    required this.onTap,
+  });
 }
